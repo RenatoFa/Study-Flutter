fca3431a56cd104924e6e3f9d1f79b00dbc080ce
Renato Ferreira de Assis <renato.assis@g.globo>
2022-09-16 20:35:22 -0300

    feat: add screen login

--- lib/login.dart
+++ lib/login.dart
@@ -8,8 +8,47 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  String email = '';
+  String password = '';
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        body: SingleChildScrollView(
+      child: SizedBox(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextField(
+                  onChanged: (text) {
+                    email = text;
+                  },
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: const InputDecoration(
+                      labelText: 'Email', border: OutlineInputBorder()),
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                TextField(
+                  onChanged: (text) {
+                    password = text;
+                  },
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                      labelText: 'Password', border: OutlineInputBorder()),
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                ElevatedButton(onPressed: () {}, child: const Text('Entrar'))
+              ],
+            ),
+          )),
+    ));
   }
 }
